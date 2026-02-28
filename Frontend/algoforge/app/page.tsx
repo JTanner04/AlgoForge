6339c79e4b928011b46a9e832d4b3377dbb64f89
JTanner04 <138722231+JTanner04@users.noreply.github.com>
2026-02-27 18:56:31 -0600

    Once again another test

--- Frontend/algoforge/app/page.tsx
+++ Frontend/algoforge/app/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 import LoginPage from "./login/login";
 
+function testCardinalDebug() {
+  console.log("This is a debug function");
+}
+
 export default function Home() {
   return(
     <LoginPage />
