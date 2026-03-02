6e20d43e721b70ba334feada80f52f3b91a40c93
JTanner04 <138722231+JTanner04@users.noreply.github.com>
2026-03-01 23:21:58 -0600

    Test

--- Frontend/algoforge/authConfig.ts
+++ Frontend/algoforge/authConfig.ts
@@ -4,18 +4,3 @@ import { fallbackModeToFallbackField } from "next/dist/lib/fallback";
 export const API_TOKEN = process.env.API_TOKEN;
 
 // aldkjaf
-
-fsfadfd
-
-fallbackModeToFallbackField
-//fds
-
-
-f;oasdklfjsa;ljkf
-
-getFlightDataPartsFromPath
-
-
-jejre
-
-keyof
